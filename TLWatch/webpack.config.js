06a6fef17d4e1498d1eea0aeaad774d0af4d8bb9
Rafael Mohlin <raffe_mohlin@hotmail.com>
2018-01-20 21:37:49 +0100

    Add less support

--- TLWatch/webpack.config.js
+++ TLWatch/webpack.config.js
@@ -19,7 +19,8 @@ module.exports = (env) => {
             rules: [
                 { test: /\.ts$/, use: isDevBuild ? ['awesome-typescript-loader?silent=true', 'angular2-template-loader', 'angular2-router-loader'] : '@ngtools/webpack' },
                 { test: /\.html$/, use: 'html-loader?minimize=false' },
-                { test: /\.css$/, use: [ 'to-string-loader', isDevBuild ? 'css-loader' : 'css-loader?minimize' ] },
+                { test: /\.less$/, use: [ 'to-string-loader', (isDevBuild ? 'css-loader' : 'css-loader?minimize'), 'less-loader?strictMath&noIeCompat'] },
+                //{ test: /\.css$/, use: [ 'to-string-loader', isDevBuild ? 'css-loader' : 'css-loader?minimize' ] },
                 { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
             ]
         },
